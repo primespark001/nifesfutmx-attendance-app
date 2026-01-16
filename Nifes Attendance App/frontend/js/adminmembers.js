23b0fce155895a0179cce355ce650056e64301ae
Prime Spark || Full Stack Dev <munachinedu4jesus123@gmail.com>
2026-01-16 13:31:33 +0100

    edited

---
+++ Nifes Attendance App/frontend/js/adminmembers.js
@@ -0,0 +1,260 @@
+async function adminServices(){
+    const loading = document.getElementById("loading");
+    loading.style.display = 'flex';
+
+    const data = await getAdmin();    
+
+    if(data){
+        if(data.admin.isLoggedIn){
+            links(data.admin.admin_id); 
+            await overview(data.admin.admin_id);
+            await todayService(data.admin.admin_id);
+            await services(data.admin.admin_id);
+            loading.style.display = 'none';
+        } else {
+            mess(false, `Please Login!`);
+            setTimeout(async () => {
+                const res = await fetch('/auth/admin-login', { method: "GET"});
+                window.location = res.url;
+            }, 4000);
+        }
+    } else {
+        mess(false, 'Unidentified user!');
+        setTimeout(async () => {
+            const res = await fetch('/auth/register', { method: "GET"});
+            window.location = res.url;
+        }, 4000);
+    }
+}
+
+adminServices();
+
+async function todayService(adminID){
+    const servConLink = document.getElementById('todayservlink');
+    const servCon = document.getElementById('todayserv');
+    servCon.innerHTML = loading();
+    try{
+        const res = await fetch(`/admin/${adminID}/todayservice`, {method: "GET"});
+        const data = await res.json();
+        const service = data.service;
+
+        if(res.ok){
+            servConLink.href = `/admin/${adminID}/ad-services/details?servID=${service.id}`;
+            servCon.innerHTML = `<img src="${service.img}" alt="Service">`;
+        } else {
+            servConLink.disabled = true;
+            servCon.innerHTML = noContent();
+        }
+    } catch (err){
+        if(err) throw new Error(err);
+    }
+}
+
+async function services(adminID){
+    const allNum = document.getElementById('allnum');
+    const allCon = document.getElementById('allcon');
+    const sunNum = document.getElementById('sunnum');
+    const sunCon = document.getElementById('suncon');
+    const tueNum = document.getElementById('tuenum');
+    const tueCon = document.getElementById('tuecon');
+    const thurNum = document.getElementById('thurnum');
+    const thurCon = document.getElementById('thurcon');
+
+    allCon.innerHTML = loading();
+    sunCon.innerHTML = loading();
+    tueCon.innerHTML = loading();
+    thurCon.innerHTML = loading();
+
+    const searchBtn = document.getElementById('searchbtn');
+    searchBtn.onclick = searchServ(adminID);
+
+    try{
+        const res = await fetch(`/admin/${adminID}/services`, {method: "GET"});
+        const data = await res.json();
+
+
+        if(res.ok){
+            const allServ = data.services;
+            const sunServ = allServ.filter(serv => serv.date.includes('Sun'));
+            const tueServ = allServ.filter(serv => serv.date.includes('Tue'));
+            const thurServ = allServ.filter(serv => serv.date.includes('Thu'));
+
+            allCon.innerHTML = '';
+            sunCon.innerHTML = '';
+            tueCon.innerHTML = '';
+            thurCon.innerHTML ='';
+            
+            allNum.innerHTML = allServ.length;
+            sunNum.innerHTML = sunServ.length;
+            tueNum.innerHTML = tueServ.length;
+            thurNum.innerHTML = thurServ.length;
+            
+            if(allServ.length == 0) allCon.innerHTML = noContent();
+            if(sunServ.length == 0) sunCon.innerHTML = noContent();
+            if(tueServ.length == 0) tueCon.innerHTML = noContent();
+            if(thurServ.length == 0) thurCon.innerHTML = noContent();
+
+            allServ.forEach(serv => {
+                const servcard = servCard(serv);
+                allCon.append(servcard);
+            });
+            sunServ.forEach(serv => {
+                const servcard = servCard(serv);
+                sunCon.append(servcard);
+            });
+            tueServ.forEach(serv => {
+                const servcard = servCard(serv);
+                tueCon.append(servcard);
+            });
+            thurServ.forEach(serv => {
+                const servcard = servCard(serv);
+                thurCon.append(servcard);
+            });            
+
+        } else {
+            allCon.innerHTML = noContent();
+            sunCon.innerHTML = noContent();
+            tueCon.innerHTML = noContent();
+            thurCon.innerHTML = noContent();
+        }
+
+    } catch (err){
+        if(err) throw new Error(err);
+    }
+}
+
+function servCard(service){
+    return `
+        <div class="tabledata" onclick='gotoServDetails(${service.id})'>
+            <div>
+                <img src="${service.img}" alt=".">
+                <p>${service.title}</p>
+            </div>
+            <b>${service.date}</b>
+            <b>${service.time}</b>
+            <b>${service.attendance.length}</b>
+            <b class="tdstatus ${service.status.toLowerCase()}">${service.status.toUpperCase()}</b>
+        </div>
+    `;
+}
+
+async function searchServ(adminID){
+    const searchInput = document.getElementById('dsearch').value.trim();
+    const searchCon = document.getElementById('searchcon');
+    const searchNum = document.getElementById('searchnum');
+
+    searchCon.innerHTML = loading();
+
+    if(!searchInput){
+        searchCon.innerHTML = noContent();
+    } else {
+        try {
+            const res = await fetch(`/admin/${adminID}/service?title=${searchInput}`, { method: 'GET'});
+            const data = await res.json();
+
+            if(res.ok){
+                const services = data.services;
+                searchNum.innerHTML = services.length;
+
+                searchCon.innerHTML = '';
+                services.forEach(serv => {
+                    const servcard = servCard(serv);
+                    searchCon.append(servcard);
+                });
+            } else{
+                searchNum.innerHTML = '0';
+                searchCon.innerHTML = noContent;
+            }
+
+        } catch (err) {
+            if (err) throw new Error(err);
+        }
+    }
+}
+
+async function gotoServDetails(servID){
+    const loading = document.getElementById("loading");
+    loading.style.display = 'flex';
+
+    const adminID = document.getElementById('admin-id').innerHTML;
+    const res = await fetch(`/admin/${adminID}/ad-services/details?servID=${servID}`, {method: 'GET'});
+    if(res.ok){
+        loading.style.display = 'none';
+        window.location = res.url;
+    } else {
+        mess(false, 'An error occured. Please try again.');
+        loading.style.display = 'none';
+    }
+}
+
+const imgInput = document.getElementById('editimg');
+imgInput.onchange = (event) => {
+    const edImg = document.getElementById('servimg');
+    edImg.src = URL.createObjectURL(event.target.files[0]);
+};
+
+async function createService(){
+    const loading = document.getElementById("loading");
+    loading.style.display = 'flex';
+
+    const adminID = document.getElementById('admin-id').innerHTML;
+
+    // These two are confusing me     
+    const imgInput = document.getElementById('editimg');
+
+    const servTitle = document.getElementById('title').value.trim();
+    const servDate = document.getElementById('date').value.trim();
+    const servTime = document.getElementById('time').value.trim();
+    const servDesc = document.getElementById('desc').value.trim();
+    const servStartTime = document.getElementById('start').value;
+    const servStopTime = document.getElementById('stop').value;
+
+    if(servTitle&&servDate&&servTime&&servDesc&&servStartTime&&servStopTime){
+        const date = new Date(servDate).toLocaleDateString('en-NG', {
+            weekday: 'short',
+            day: '2-digit',
+            month: 'short',
+            year: 'numeric'
+        });
+
+        let [hours, minutes] = servTime.split(':').map(Number);
+        const meridian = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12 || 12;
+
+        const time = `${hours}:${String(minutes).padStart(2, '0')} ${meridian}`;
+        const servid = String(Date.now());
+
+        try{
+            const res = await fetch(`/admin/${adminID}/create-service`, {
+                method: 'POST',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    id: servid,
+                    title: servTitle,
+                    date: date,
+                    time: time,
+                    imgFile: imgInput.files[0],
+                    descrip: servDesc,
+                    start: servStartTime,
+                    stop: servStopTime,    
+                })
+            });
+            const data = await res.json();
+            if(res.ok){
+                mess(true, data.message);
+                setTimeout(() => {
+                    adminServices();
+                    loading.style.display = 'none';
+                }, 4000);
+            }
+
+        } catch (error){
+            if(error) throw new Error(error)
+        }
+
+    } else {
+        mess(false, 'Please complete the form!')
+        loading.style.display = 'none';
+    }
+
+}
---
+++ Nifes Attendance App/frontend/js/memberDetails.js
@@ -0,0 +1,176 @@
+async function serviceDetails(){
+    const loading = document.getElementById("loading");
+    loading.style.display = 'flex';
+
+    const data = await getAdmin();    
+
+    if(data){
+        if(data.admin.isLoggedIn){
+            links(data.admin.admin_id);
+            await servDetails(data.admin.admin_id);
+            loading.style.display = 'none';
+        } else {
+            mess(false, `Please Login!`);
+            setTimeout(async () => {
+                const res = await fetch('/auth/admin-login', { method: "GET"});
+                window.location = res.url;
+            }, 4000);
+        }
+    } else {
+        mess(false, 'Unidentified user!');
+        setTimeout(async () => {
+            const res = await fetch('/auth/register', { method: "GET"});
+            window.location = res.url;
+        }, 4000);
+    }
+}
+
+serviceDetails();
+
+async function servDetails(adminID){
+    const params = new URLSearchParams(window.location.search);
+    const serviceID = params.get(servID);
+    
+    const serviceCon = document.getElementById('dservice');
+    serviceCon.innerHTML = loading();
+
+    const delBtn = document.getElementById('delbtn');
+    delBtn.onclick = deleteService(adminID, serviceID);
+
+    try{
+        
+        const res = await fetch(`/admin/${adminID}/service?servID=${serviceID}`, {method: 'GET'});
+        const data = await res.json();
+        const service = data.service;        
+        
+        if(res.ok){
+            const membersRes = await fetch(`/admin/${adminID}/members/total`, {method: 'GET'});
+            const membersData = membersRes.json();
+            const totalMembers = membersData.members.length;
+
+            const rate = Math.round(((service.attendance.length / totalMembers) * 100) * 100) / 100;
+            serviceChart(rate);
+            await servMembers(adminID, service.attendance);
+
+            serviceCon.innerHTML  = serviceInfo(service, totalMembers, rate);
+        } else {
+            serviceCon.innerHTML = noContent();
+        }
+
+    } catch (err){
+        if(err) throw new Error(err);
+    }
+}
+
+function serviceInfo(service, totalMembers, rate){
+    return `
+        <a href="${service.img}" title="Service"><img src="${service.img}" alt="Service"></a>
+        <div>
+            <p><b>Title</b>  <span>${service.title}</span></p>
+            <p><b>Date</b> <span>${service.date}</span></p>
+            <p><b>Time</b> <span>${service.time}</span></p>
+            <p><b>Attendance</b> <span>${service.attendance.length}/${totalMembers}</span></p>
+            <p><b>Rate</b> <span>${rate}%</span></p>
+            <p><b>Status</b> <span class="${service.status.toLowerCase()}">${service.status.toUpperCase()}</span></p>
+        </div>
+        <div>
+            <p><b>Description</b> 
+                <span>${service.descrip}</span>
+            </p>
+        </div>
+    `;
+}
+
+function serviceChart(rate){
+    const bar = document.getElementById('bar');
+    const barvalue = document.getElementById('barvalue');
+
+    bar.style.height = `${rate}%`;
+    barvalue.style.height = `${rate}%`;
+    barvalue.textContent = `${rate}%`;
+}
+
+async function servMembers(adminID, membersIds){
+    const totalNum = document.getElementById('totalnum');
+    const totalCon = document.getElementById('totalcon');
+    const searchBtn = document.getElementById('searchattendmembers');
+    
+    let members = [];
+    totalNum.innerHTML = membersIds.length;
+    membersIds.forEach(userID, async() => {
+        try{
+            const res = await fetch(`/admin/${adminID}/member/${userID}`, {method: 'GET'});
+            const data = res.json();
+            if(res.ok){
+                const user = data.user;
+                const profile = profilecard(adminID, user, 'pc2')
+                totalCon.append(profile);
+                members.push(profile);
+            }
+        } catch (error){
+            if(err) throw new Error(error);
+        }   
+    
+    });
+
+    serachBtn.onclick = searchAttMem(adminID, members);
+
+}
+
+function searchAttMem(adminID, memebrs){
+    const input = document.getElementById('dmsearch');
+    const resultNum = document.getElementById('resultnum');
+    const resultCon = document.getElementById('resultcon');
+
+    const name  = input.value.trim();
+
+    resultCon.innerHTML = loading();
+    if(name){
+        resultCon.innerHTML = ''
+        const result = members.filter(user =>
+            user.surname.includes(name) ||
+            user.firstname.includes(name) ||
+            user.othername.includes(name)
+        );
+        if(result){
+            result.forEach(user => {
+                const profile = profilecard(adminID, user, 'pc2')
+                resultCon.append(profile);    
+            });
+            resultNum.innerHTML = result.length;
+        } else {
+            resultCon.innerHTML = noContent();
+            resultNum.innerHTML = '0';
+        }
+    } else {
+        mess(false, "Enter member's name");
+        resultCon.innerHTML = noContent();
+    }    
+}
+
+async function deleteService(adminId, servId) {
+    const loading = document.getElementById("loading");
+    loading.style.display = 'flex';    
+
+    try {
+        const res = await fetch(`/admin/${adminID}/delete-service`, {
+            method: 'DELETE',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ 
+                adminID: adminId,
+                servID: servId
+            })
+        });
+        const data = await res.json();
+        if(res.ok){
+            mess(true, data.message);
+            loading.style.display = 'none';
+        } else{
+            mess(false, data.message);
+            loading.style.display = 'none';
+        }
+    } catch (error) {
+        if(error) throw new Error(error); 
+        loading.style.display = 'none';       
+    }
+}
